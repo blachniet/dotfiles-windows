bf3969e6b7034624957bfc0879399d40817a9781
Brian Lachniet <blachniet@gmail.com>
2016-09-14 08:10:16 -0400

    Remove git symlink file

--- git/symlink.ps1
+++
@@ -1,6 +0,0 @@
-$link = (Join-Path $HOME .gitconfig)
-if (Test-Path $link){
-  mv $link "$link.$(Get-Date -f yyMMddhhmmss).old"
-}
-
-iex "cmd /c mklink /H `"$link`" `"$(Join-Path $PSScriptRoot .gitconfig)`""
